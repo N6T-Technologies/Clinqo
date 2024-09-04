c9787c6e11a5f833efc92639600c123b8c08433c
Meru45 <mayurpachpor2802@gmail.com>
2024-09-04 19:44:56 +0530

    Improve style of Console and Data-Table

--- apps/hospital-client/app/(protected)/console/@admin/page.tsx
+++ apps/hospital-client/app/(protected)/console/@admin/page.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export default function Console() {
     return (
-        <div className="h-full w-full grid grid-cols-2 p-4 space-x-4">
+        <div className="h-[calc(100%-4rem)] w-full grid grid-cols-2 p-4 space-x-4">
             <div className="h-full space-y-4">
                 <Card className="h-[calc(50%-0.5rem)] border border-gray-300 shadow-md hover:shadow-lg hover:bg-slate-50">
                     <CardHeader className="text-2xl font-bold">Revenue</CardHeader>
